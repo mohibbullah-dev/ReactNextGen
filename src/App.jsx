1dd4b0292371523d3de13c177a9ced4371220227
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-02-06 21:05:36 +0200

    marketplace page correction

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import RootLayouts from "./layouts/RootLayouts";
 import Artist from "./pages/Artist";
 import Community from "./pages/Community";
 import Home from "./pages/Home";
-import Marketplace from "./pages/Marketplace";
+import MarketPlace from "./pages/MarketPlace";
 function App() {
   return (
     <>
       <Routes>
         <Route element={<RootLayouts />}>
           <Route path="/" element={<Home />} />
-          <Route path="/marketplace" element={<Marketplace />} />
+          <Route path="/marketplace" element={<MarketPlace />} />
           <Route path="/artist" element={<Artist />} />
           <Route path="/community" element={<Community />} />
         </Route>
--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../sections/Navbar";
 
-const Marketplace = () => {
+const MarketPlace = () => {
   return (
     <>
       <Navbar />
@@ -10,4 +10,4 @@ const Marketplace = () => {
   );
 };
 
-export default Marketplace;
+export default MarketPlace;
